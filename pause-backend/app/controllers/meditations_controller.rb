800a3778ff949be48baf0347fdeb4b5ba053d255
Jessie <jssdnvn.com>
2021-01-26 20:14:33 -0600

    added seed data

--- pause-backend/app/controllers/meditations_controller.rb
+++ pause-backend/app/controllers/meditations_controller.rb
@@ -1,11 +1,12 @@
 class MeditationsController < ApplicationController
    def index 
       meditations = Meditation.all
-      render json: meditations, include [:ratings]
+      render json: meditations # include [:ratings]
+   end
 
    def show 
       meditation = Meditation.find_by_id(params[:id])
-      render json: meditation, include [:ratings]
+      render json: meditation #include [:ratings]
    end
 
 end
--- pause-backend/db/seeds.rb
+++ pause-backend/db/seeds.rb
@@ -1,4 +1,19 @@
 Meditation.delete_all 
-Rating.delete_all 
 
 # create meditation data
+
+Meditaton.delete_all
+
+meditation_title = [
+  'Stress Relief',
+  'Handling Anxiety',
+  'Pain Management',
+  'Affirmations',
+  'Breathe',
+]
+
+meditation_collection = []
+
+meditation_title.each do |title|
+  meditation_collection << Meditation.create(title: title)
+end
