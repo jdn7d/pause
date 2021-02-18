e0be886411ab7bd47364fbc633fbdfac3a114338
Jessie <jssdnvn.com>
2021-02-18 09:59:09 -0600

    re seeded with full meditations

--- pause-backend/db/seeds.rb
+++ pause-backend/db/seeds.rb
@@ -1,35 +1,26 @@
 Meditation.delete_all 
 
 Meditation.create!([{
-   title: "Breathing    ",
+   title: "Breathing",
    duration: 3,
-   content: "Inhale/Exhale/Inhale/Exhale" },
+   content: "Inhale through your nose/Exhale through your mouth/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale/Inhale/Exhale" },
 
    {
    title: "Handling Anxiety    ",
    duration: 5,
-   content: "Begin in a comfortable seated position/Feel rooted, safe, and comfortable through your seat/Once you’re settled, soften your gaze/Relax the muscles in your face/Take a deep breath in through your nose/And out through your mouth/1/2/3/4/As you inhale, imagine that you are breathing in calmness/As you exhale, imagine that you are breathing out your anxiety/Inhale peace and clarity/Exhale stress and worry/
-   Inhale/Feel your anxiety slip away with each exhale/
-   Inhale/Exhale/
-   Your mind may drift to feelings of worry/This is okay, be mindful/
-   Without judgment, gently bring your attention back to the breath/Notice how calm you feel as you sit and breathe"},
+   content: "Begin in a comfortable seated position/Feel rooted, safe, and comfortable through your seat/Once you’re settled, soften your gaze/Relax the muscles in your face/Take a deep breath in through your nose/And out through your mouth/1/2/3/4/As you inhale, imagine that you are breathing in calmness/As you exhale, imagine that you are breathing out your anxiety/Inhale peace and clarity/Exhale stress and worry/Inhale/Feel your anxiety slip away with each exhale/Inhale/Exhale/Your mind may drift to feelings of worry/This is okay, be mindful/Without judgment, gently bring your attention back to the breath/Notice how calm you feel as you sit and breathe"},
    {
    title: "Pain Management    ",
    duration: 3,
-   content: "Take a breath in/Exhale/Settle into a comfortable position/Notice how you are feeling in this moment/Observe your body and mind/
-   Pain management begins with observation./Where is most of your tension stored?/Where is your pain located?/What part of your body is most relaxed?/Take a deep breath in.... now exhale./
-   Breathe in.... and out./Continue to breathe slowly, smoothly./Now continue the pain management relaxation with a passive attitude of observing. Do not try to make anything happen. 
-   Notice how your whole body feels. Passively observe, not trying to change anything. Simply take note of how your body feels. Take a few moments now to think about the pain you experience.
-   You may not be in pain right now. Just observe the state of your pain in this moment. The way your body feels is always changing. The way you feel is different from moment to moment. 
-   A moment from now, you will feel slightly different from the way you feel right now. Just observe. Observe as each moment passes. 
-   Although pain is unwanted and difficult to tolerate, try for the next few moments to regard your pain with acceptance. 
-   Accept the way you are feeling right now physically and emotionally..... whether positive or negative..... allow your body and mind to just" },
+   content: "Take a breath in/Exhale/Settle into a comfortable position/Notice how you are feeling in this moment/Observe your body and mind/Pain management begins with observation./Where is most of your tension stored?/Where is your pain located?/What part of your body is most relaxed?/Take a deep breath in.... now exhale./Breathe in.... and out./Continue to breathe slowly./Now continue with a passive attitude of observing./Notice how your whole body feels/Passively observe any pain/Simply take note of how your body feels/Focus on the pain you have/Pinpoint the areas of you body/You may not be in pain right now./Just observe the state of your pain in this moment./The way your body feels is always changing./Just observe./Observe as each moment passes./Although pain is unwanted and difficult to tolerate,/Try for the next few breaths to regard your pain with acceptance./Accept the way you are feeling right now physically and emotionally/..... whether positive or negative..... allow your body and mind to just" },
    {
-   title: "Affirmations",
-   duration: 3 },
+   title: "Gratitude",
+   duration: 2,
+   content: "Begin in a comfortable seated position./Feel grounded through your seat, and when you’re settled, close your eyes./This guided meditation script will help you feel more gratitude./Start to pay attention to your breath by taking mindful breaths in through your nose and out through your mouth./Breathe in for the count of four and out for the count of four./Start to think about how lucky you are./What are you grateful for?/Take a moment to name at least three things you are grateful for./As you inhale, send extra gratitude to these things./If your mind starts to wander to other things, simply be an observer of your thoughts and do not judge them./Then, bring attention back to your breath and the things you’re grateful for./Keep breathing. All you need to do right now is breathe and drench yourself in gratitude./Notice how practicing mindfulness and gratitude can help you feel happier./Open your eyes and take this amazing feeling of gratitude with you throughout your day."  },
    {
-   title: "Breathe",
-   duration: 1
+   title: "Test",
+   duration: 1,
+   content: "1/2/3"
 }])
 
 
